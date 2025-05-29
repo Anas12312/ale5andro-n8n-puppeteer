8beee2112cd4e8d11c3556cc411cdd7aed55946d
zyad007 <zyad.sallem.007@gmail.com>
2025-05-30 00:52:06 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import express, { Request, Response, RequestHandler } from 'express'
+import puppeteer, { Page } from "puppeteer-core";
+import cors from 'cors'
+import dotenv from 'dotenv'
+dotenv.config()
+
+import scrape from './scrape'
+
+// Create Express app
+const app = express()
+
+// Middleware
+app.use(cors())
+app.use(express.json())
+
+// GET endpoint
+const handleRequest: RequestHandler = async (req, res) => {
+    const { id, year, month } = req.query
+
+    if (!id || !year || !month) {
+        res.status(400).json({ error: 'Missing required parameters' })
+        return
+    }
+
+    try {
+        const result = await scrape(id as string, year as string, month as string)
+        res.json(result)
+        return
+    } catch (error) {
+        console.error('Error in handleRequest:', error)
+        res.status(500).json({ error: 'An error occurred' })
+        return
+    }
+}
+
+app.get('/', handleRequest)
+
+// Start server
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
---
+++ src/scrape.ts
@@ -0,0 +1,85 @@
+import puppeteer, { Page } from "puppeteer-core"
+
+
+interface Data {
+    payroll_number: string
+    payroll_type: string
+    payroll_value: string
+    state: string
+    settled_period: string
+}
+
+export default async function scrape(id: string, year: string, month: string): Promise<Data[]> {
+
+    try {
+        const BROWSER_WS_ENDPOINT = process.env.BROWSER_WS_ENDPOINT
+
+        const browser = await puppeteer.connect({
+            browserWSEndpoint: BROWSER_WS_ENDPOINT
+        });
+
+
+        const page: Page = await browser.newPage();
+        await page.goto("https://servicio.nuevosoi.com.co/soi/consultarplanillas.do", { waitUntil: "networkidle0" });
+
+        const firstSelect = await page.waitForSelector('select#tipoDocumento');
+
+        await firstSelect?.select('1');
+
+        const idInput = await page.waitForSelector('input#numeroDocumento');
+
+        await idInput?.type(id);
+
+        // Search 1
+        const searchButton = await page.waitForSelector('a#planillasDisponiblesPago')
+        await searchButton?.click();
+
+
+        // Select Year
+        const secondSelect = await page.waitForSelector('select#periodoLiqOtrosSubsAnno')
+        await secondSelect?.select(year);
+
+        // Select Month
+        const thirdSelect = await page.waitForSelector('select#periodoLiqOtrosSubsMess')
+        await thirdSelect?.select(month);
+
+        // Search 2
+        const searchButton2 = await page.waitForSelector('button#btnGuardar')
+        await searchButton2?.click();
+
+        try {
+            // Table
+
+            await page.waitForNetworkIdle()
+
+            const table = await page.waitForSelector('table#tablaPlanillaAsistida', { timeout: 1_000 })
+
+            const tableRows = await table?.evaluate(el => {
+                const rows = el.querySelectorAll('tr');
+
+                return Array.from(rows).map(row => {
+                    const cells = row.querySelectorAll('td');
+                    const arr = Array.from(cells).map(cell => cell.textContent);
+                    return {
+                        payroll_number: arr[0],
+                        payroll_type: arr[1],
+                        payroll_value: arr[2],
+                        state: arr[3],
+                        settled_period: arr[4]?.trim(),
+                    }
+                });
+            })
+
+            tableRows?.shift()
+
+            return tableRows as Data[] || []
+        } catch (error) {
+            console.error('No data found')
+            return []
+        }
+
+    } catch (error) {
+        console.error('Error in scrape:', error)
+        return []
+    }
+}
